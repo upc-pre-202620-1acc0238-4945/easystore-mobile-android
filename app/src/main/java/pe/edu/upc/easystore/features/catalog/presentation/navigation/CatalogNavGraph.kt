c140d773b16c9a419df737c078478020522d3d7d
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-18 11:49:24 -0500

    feat(catalog): update catalog navigation to use classes for routes

--- app/src/main/java/pe/edu/upc/easystore/features/catalog/presentation/navigation/CatalogNavGraph.kt
+++ app/src/main/java/pe/edu/upc/easystore/features/catalog/presentation/navigation/CatalogNavGraph.kt
@@ -3,18 +3,24 @@ package pe.edu.upc.easystore.features.catalog.presentation.navigation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import kotlinx.serialization.Serializable
 import pe.edu.upc.easystore.features.catalog.presentation.home.HomeScreen
 import pe.edu.upc.easystore.features.catalog.presentation.productdetail.ProductDetailScreen
 
+@Serializable
+object CatalogRoute
+
+@Serializable
+data class ProductDetailRoute(val id: Int)
 
 fun NavGraphBuilder.catalogNavGraph(navController: NavController) {
 
 
-    composable("home") {
+    composable<CatalogRoute> {
         HomeScreen()
     }
 
-    composable("product_detail/{id}") {
+    composable<ProductDetailRoute> {
 
     }
 }
