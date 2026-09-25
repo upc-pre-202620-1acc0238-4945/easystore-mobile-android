2e1caa7c101d94706e58d15a08ba2d544dfd8756
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-25 12:08:35 -0500

    chore: update Room imports to version 3 for compatibility

--- app/src/main/java/pe/edu/upc/easystore/features/catalog/infrastructure/local/ProductDao.kt
+++ app/src/main/java/pe/edu/upc/easystore/features/catalog/infrastructure/local/ProductDao.kt
@@ -1,9 +1,9 @@
 package pe.edu.upc.easystore.features.catalog.infrastructure.local
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room3.Dao
+import androidx.room3.Delete
+import androidx.room3.Insert
+import androidx.room3.Query
 
 @Dao
 interface ProductDao {
--- app/src/main/java/pe/edu/upc/easystore/features/catalog/infrastructure/local/ProductEntity.kt
+++ app/src/main/java/pe/edu/upc/easystore/features/catalog/infrastructure/local/ProductEntity.kt
@@ -1,8 +1,8 @@
 package pe.edu.upc.easystore.features.catalog.infrastructure.local
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room3.ColumnInfo
+import androidx.room3.Entity
+import androidx.room3.PrimaryKey
 
 @Entity(tableName = "products")
 data class ProductEntity(
